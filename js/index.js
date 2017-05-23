7bc2d1854e3960702e0d5d4a3f6c8262586061dd
k54wukai <k54wu@edu.uwaterloo.ca>
2017-05-22 23:51:46 -0400

    sdfds

--- js/index.js
+++ js/index.js
@@ -24,3 +24,79 @@ $(document).ready(function(){
         $(".current").find(".details").slideDown(200);
     });
  });   
+
+ window.onload=function ()
+    {
+        var aDiv=document.getElementsByClassName('div1');
+        var aBtn=document.getElementsByClassName('nav');
+        //var aBt=aDiv;
+        for(var i=0;i<aDiv.length;i++)
+        {
+            aDiv[i].onmouseover = function(){
+                startMove(this, 'width', 700);
+            }
+            aDiv[i].onmouseout = function(){
+                startMove(this, 'width', 100);
+            }
+        }
+        
+        aBtn.onmouseover = function(){
+           startMove(this, 'width', 150);
+        }
+        aBtn.onmouseout = function(){
+            startMove(this,'width', 100);
+        }
+    };
+
+
+    // 这个直接在style 上做出改动，就不需要offset了
+    function getStyle(obj, name)
+    {
+        if(obj.currentStyle)
+        {
+            return obj.currentStyle[name];
+        }
+        else
+        {
+            return getComputedStyle(obj, false)[name];
+        }
+    }
+
+    function startMove(obj, attr, iTarget)
+    {
+        clearInterval(obj.timer);
+        obj.timer=setInterval(function (){
+            var cur=0;
+
+            if(attr=='opacity')
+            {
+                cur=Math.round(parseFloat(getStyle(obj, attr))*100);
+            }
+            else
+            {
+                cur=parseInt(getStyle(obj, attr));
+            }
+
+            var speed=(iTarget-cur)/6;
+            speed=speed>0?Math.ceil(speed):Math.floor(speed);
+
+            if(cur==iTarget)
+            {
+                clearInterval(obj.timer);
+            }
+            else
+            {
+                if(attr=='opacity')
+                {
+                    obj.style.filter='alpha(opacity:'+(cur+speed)+')';
+                    obj.style.opacity=(cur+speed)/100;
+
+                    document.getElementById('txt1').value=obj.style.opacity;
+                }
+                else
+                {
+                    obj.style[attr]=cur+speed+'px';
+                }
+            }
+        }, 30);
+    }
